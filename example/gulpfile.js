1b959d1ce1a6b76cc45c3f8bbdbf495542faa8ef
mrbaloo <bernhard.straehle@gmail.com>
2015-01-01 14:29:17 +0000

    removed example for image resize

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -5,37 +5,6 @@ var c = require('./gulp/config');
 
 var requireDir = require('require-dir');
 var dir = requireDir('./gulp/tasks');
-var gm = require("gm");
 
-gulp.task('scaleimages', function() {
-    // gm('target/img/fahrschule-kiy-logo.jpg')
-    //     .resize(null, 92)
-    //     .write('target/img/fahrschule-kiy-logo.default.jpg', function(err) {
-    //         if(err){
-    //             console.log(err);    
-    //         } else {
-    //             console.log('done');
-    //         }
-    //     });
-    // gm('target/img/fahrschule-kiy-logo.jpg')
-    //     .resize(null, 60)
-    //     .write('target/img/fahrschule-kiy-logo.xs.jpg', function(err) {
-    //         if(err){
-    //             console.log(err);    
-    //         } else {
-    //             console.log('done');
-    //         }
-    //     });
-        
-     gm('target/img/fritz-kiy-square.jpg')
-        .resize(null, 200)
-        .write('target/img/fritz-kiy-square.jpg', function(err) {
-            if(err){
-                console.log(err);    
-            } else {
-                console.log('done');
-            }
-        });
-});
 
 gulp.task('default', ['devtools:browser-sync']);
